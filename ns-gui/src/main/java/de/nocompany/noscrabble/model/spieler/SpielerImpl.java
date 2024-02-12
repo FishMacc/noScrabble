869778f8b641ee836a56e800dac01200735a2c5c
AlexPC <mytrashmail0100@gmail.com>
2024-02-12 11:34:15 +0100

    ohne Interface

--- ns-gui/src/main/java/de/nocompany/noscrabble/model/spieler/SpielerImpl.java
+++ ns-gui/src/main/java/de/nocompany/noscrabble/model/spieler/SpielerImpl.java
@@ -3,52 +3,43 @@ package de.nocompany.noscrabble.model.spieler;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SpielerImpl implements Spieler{
+public class SpielerImpl{
     private String name;
     private List<String> buchstabenRack = new ArrayList<>();
     private int punkte = 0;
     private boolean turn = false;
-    @Override
     public void setName(String name) {
         this.name=name;
     }
 
-    @Override
     public String getName() {
         return name;
     }
 
-    @Override
     public void addBuchstaben(List<String> neueBuchstaben) {
         buchstabenRack.addAll(neueBuchstaben);
     }
 
-    @Override
     public void removeBuchstaben(List<String> verwendeteBuchstaben) {
         buchstabenRack.removeAll(verwendeteBuchstaben);
     }
 
-    @Override
     public void updatePunkte(int addpunkte) {
         this.punkte+=addpunkte;
     }
 
-    @Override
     public int getPunkte() {
         return this.punkte;
     }
 
-    @Override
     public List<String> getBuchstabenRack() {
         return this.buchstabenRack;
     }
 
-    @Override
     public void setTurn(boolean isTurn) {
         turn=isTurn;
     }
 
-    @Override
     public boolean isTurn() {
         return turn;
     }
