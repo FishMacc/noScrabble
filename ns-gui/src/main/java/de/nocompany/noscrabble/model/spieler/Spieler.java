f940e2462de59fc285b8155a0fb8ae5387ca8f6e
AlexPC <mytrashmail0100@gmail.com>
2024-02-12 14:48:40 +0100

    getBuchstaben Methode hinzugefügt givebackBuchstaben Methode hinzugefügt Bei Initialisierung ist Pool ein übergabeparameter damit alle auf den gleichen Buchstabenpool zugreifen

--- ns-gui/src/main/java/de/nocompany/noscrabble/model/spieler/Spieler.java
+++ ns-gui/src/main/java/de/nocompany/noscrabble/model/spieler/Spieler.java
@@ -1,13 +1,20 @@
 package de.nocompany.noscrabble.model.spieler;
 
+import de.nocompany.noscrabble.model.spielsteinpool.SpielsteinPool;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Spieler{
     private String name;
-    private List<String> buchstabenRack = new ArrayList<>();
+    private List<Character> buchstabenRack = new ArrayList<>();
     private int punkte = 0;
     private boolean turn = false;
+    private SpielsteinPool pool;
+    Spieler(SpielsteinPool pool){
+        this.pool = pool;
+        getBuchstaben();
+    }
     public void setName(String name) {
         this.name=name;
     }
@@ -16,11 +23,11 @@ public class Spieler{
         return name;
     }
 
-    public void addBuchstaben(List<String> neueBuchstaben) {
+    public void addBuchstaben(List<Character> neueBuchstaben) {
         buchstabenRack.addAll(neueBuchstaben);
     }
 
-    public void removeBuchstaben(List<String> verwendeteBuchstaben) {
+    public void removeBuchstaben(List<Character> verwendeteBuchstaben) {
         buchstabenRack.removeAll(verwendeteBuchstaben);
     }
 
@@ -32,7 +39,7 @@ public class Spieler{
         return this.punkte;
     }
 
-    public List<String> getBuchstabenRack() {
+    public List<Character> getBuchstabenRack() {
         return this.buchstabenRack;
     }
 
@@ -42,5 +49,17 @@ public class Spieler{
 
     public boolean isTurn() {
         return turn;
+    }
+
+    public void getBuchstaben() {
+        int steine = 7 - buchstabenRack.size();
+        for (int i = 0; i < steine; i++) {
+            buchstabenRack.add(pool.getBuchstabe());
+        }
+    }
+
+    public void givebackBuchstabe(int index){
+        pool.addBuchstabe(buchstabenRack.get(index));
+        buchstabenRack.remove(index);
     }
 }
