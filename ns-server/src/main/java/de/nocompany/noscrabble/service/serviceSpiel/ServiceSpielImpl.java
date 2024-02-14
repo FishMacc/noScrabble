0d2776b23b0bf5d2bac67d85e2e989b112f0664b
Dietrich <waldschmidt92@googlemail.com>
2024-02-14 13:28:18 +0100

    Anfang Interface

--- ns-server/src/main/java/de/nocompany/noscrabble/service/serviceSpiel/ServiceSpielImpl.java
+++ ns-server/src/main/java/de/nocompany/noscrabble/service/serviceSpiel/ServiceSpielImpl.java
@@ -1,4 +1,30 @@
 package de.nocompany.noscrabble.service.serviceSpiel;
 
-public class ServiceSpielImpl {
+import java.util.List;
+
+public class ServiceSpielImpl implements ServiceSpielInterface {
+    @Override
+    public void neuesSpiel(List<String> spielerListe) {
+        spielsteinPool = new S
+    }
+
+    @Override
+    public void pruefeWoerter(Character[][] spielbrett) {
+
+    }
+
+    @Override
+    public void passeRunde() {
+
+    }
+
+    @Override
+    public void ersetzeBuchstaben() {
+
+    }
+
+    @Override
+    public void beendeSpiel() {
+
+    }
 }
--- ns-server/src/main/java/de/nocompany/noscrabble/service/serviceSpiel/ServiceSpielInterface.java
+++ ns-server/src/main/java/de/nocompany/noscrabble/service/serviceSpiel/ServiceSpielInterface.java
@@ -1,4 +1,17 @@
 package de.nocompany.noscrabble.service.serviceSpiel;
 
+import java.util.List;
+
 public interface ServiceSpielInterface {
+
+    void neuesSpiel(List<String> spielerListe);
+
+    void pruefeWoerter(Character[][] spielbrett);
+
+    void passeRunde();
+
+    void ersetzeBuchstaben();
+
+    void beendeSpiel();
+
 }
