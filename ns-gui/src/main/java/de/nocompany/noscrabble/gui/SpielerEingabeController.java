a1c507f76df2c29be8ee4f5fef1a591b7452be5a
Dietrich <waldschmidt92@googlemail.com>
2024-02-14 15:25:51 +0100

    Spieler eingabe controller

--- ns-gui/src/main/java/de/nocompany/noscrabble/gui/SpielerEingabeController.java
+++ ns-gui/src/main/java/de/nocompany/noscrabble/gui/SpielerEingabeController.java
@@ -1,6 +1,5 @@
 package de.nocompany.noscrabble.gui;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,6 +10,7 @@ import java.util.List;
 
 public class SpielerEingabeController {
 
+    public Button bSpielStarten;
     @FXML
     private Label lVorhandeneSpieler;
     @FXML
@@ -24,33 +24,59 @@ public class SpielerEingabeController {
     @FXML
     private TextField tfSpielername;
     @FXML
+    private Button bSpielerHinzufuegen;
+
+
+    @FXML
     private List<String> spielerListe = new ArrayList<>();
 
+    private int spielerZaehler = 0;
 
     @FXML
     void initialize() {
 
-        spielerListe.add(lSpieler1.getText());
-        spielerListe.add(lSpieler2.getText());
-        spielerListe.add(lSpieler3.getText());
-        spielerListe.add(lSpieler4.getText());
-        Button bSpielerHinzufuegen = new Button();
         bSpielerHinzufuegen.setOnAction(e -> doAddSpieler());
-
+        bSpielStarten.setOnAction(e ->doSpielStarten());
     }
 
 
     public void doAddSpieler() {
-        for (String spielerName : spielerListe) {
-            if (spielerName.isEmpty()) {
-                spielerName = tfSpielername.getText();
-                break;
-            }
+        if (spielerZaehler == 0) {
+            lSpieler1.setText(tfSpielername.getText());
+        } else if (spielerZaehler == 1) {
+            lSpieler2.setText(tfSpielername.getText());
+        } else if (spielerZaehler == 2) {
+            lSpieler3.setText(tfSpielername.getText());
+        } else if (spielerZaehler == 3) {
+            lSpieler4.setText(tfSpielername.getText());
         }
+        spielerZaehler++;
 
+        tfSpielername.setText("");
+        tfSpielername.requestFocus();
 
+        // Verhindere das Hinzufügen von mehr als 4 Spielern
+        if (spielerZaehler >= 4) {
+            bSpielerHinzufuegen.setDisable(true);
+        }
     }
 
-    public void doSpielStarten(ActionEvent actionEvent) {
+    public void doSpielStarten () {
+        if (!lSpieler1.getText().isEmpty()) {
+            spielerListe.add(lSpieler1.getText());
+        }
+        if (!lSpieler2.getText().isEmpty()) {
+            spielerListe.add(lSpieler2.getText());
+        }
+        if (!lSpieler3.getText().isEmpty()) {
+            spielerListe.add(lSpieler3.getText());
+        }
+        if (!lSpieler4.getText().isEmpty()) {
+            spielerListe.add(lSpieler4.getText());
+        }
+        //todo aus der serviceimpl ein neues spiel erstellen
+        //Spiel = new neuesSpiel(spielerListe);
+        System.out.println("Starte Programm");
     }
-}
+
+    }
