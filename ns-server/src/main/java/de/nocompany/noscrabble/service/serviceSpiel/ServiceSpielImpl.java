a8268b02b11f1f753cab044c4157ab41a32bf5a6
Dietrich <waldschmidt92@googlemail.com>
2024-02-14 14:11:31 +0100

    Service mit constructor ausgestattet

--- ns-server/src/main/java/de/nocompany/noscrabble/service/serviceSpiel/ServiceSpielImpl.java
+++ ns-server/src/main/java/de/nocompany/noscrabble/service/serviceSpiel/ServiceSpielImpl.java
@@ -1,15 +1,44 @@
 package de.nocompany.noscrabble.service.serviceSpiel;
 
+import de.nocompany.noscrabble.model.leaderboard.Leaderboard;
+import de.nocompany.noscrabble.model.spieler.Spieler;
+import de.nocompany.noscrabble.model.spielsteinpool.SpielsteinPool;
+import de.nocompany.noscrabble.service.serviceSpielbrett.ServiceSpielbrettImpl;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceSpielImpl implements ServiceSpielInterface {
-    @Override
-    public void neuesSpiel(List<String> spielerListe) {
-        spielsteinPool = new S
+    private SpielsteinPool spielsteinPool;
+    private final ArrayList<Spieler> spielerListe;
+    private Leaderboard leaderBoard;
+
+    private ServiceSpielbrettImpl serviceSpielbrett;
+
+    public ServiceSpielImpl() {
+        this.spielerListe = new ArrayList<Spieler>();
     }
 
     @Override
-    public void pruefeWoerter(Character[][] spielbrett) {
+    public void neuesSpiel(List<String> spielerNamen) {
+        //SpielsteinPool wird erstellt
+        spielsteinPool = new SpielsteinPool();
+        //Spieler werden erstellt
+        for (String name : spielerNamen) {
+            Spieler spieler = new Spieler(spielsteinPool);
+            spieler.setName(name);
+            spielerListe.add(spieler);
+        }
+        //Startspieler setzen
+        spielerListe.getFirst().setTurn(true);
+        //erstelle Leaderboard
+        leaderBoard = new Leaderboard(spielerListe);
+        //erstelle Spielfeld
+        serviceSpielbrett = new ServiceSpielbrettImpl();
+
+
+    @Override
+    public void pruefeWoerter() {
 
     }
 
