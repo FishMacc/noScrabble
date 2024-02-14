e802075f72f3f6ee6d986521e99f15f3857bac3b
Dietrich <waldschmidt92@googlemail.com>
2024-02-14 10:58:55 +0100

    Starter angepasst

--- ns-gui/src/main/java/de/nocompany/noscrabble/gui/StarterApp.java
+++ ns-gui/src/main/java/de/nocompany/noscrabble/gui/StarterApp.java
@@ -6,37 +6,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class StarterApp extends Application {
+    BorderPane root = new BorderPane();
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Starter s = new Starter();
-        Label l = new Label("Hallo noScrabble! " + s.getVersion());
-        HBox root = new HBox();
-        root.getChildren().add(l);
-        //Scene scene = new Scene(root, 300, 400);
-        //primaryStage.setScene(scene);
-        //primaryStage.show();
+        primaryStage.setTitle("noScrabble");
         showStartseiteHintergrund();
+        primaryStage.setScene(new Scene(root, 1024, 1024));
         showSpielerEingabe();
     }
 
     private void showStartseiteHintergrund() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/startseiteHintergrund.fxml"));
-            Parent root = fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.setTitle("noScrabble");
-            stage.setScene(new Scene(root, 1024, 1024));
-            stage.show();
+            Parent hintergrundContent = fxmlLoader.load();
+            root.setCenter(hintergrundContent);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -47,9 +41,7 @@ public class StarterApp extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/spielerEingabe.fxml"));
             Parent eingabeContent = fxmlLoader.load();
             //Stage stage = new Stage();
-            if (root instanceof HBox) {
-                ((HBox) root).setCenter(eingabeContent);
-            }
+            root.setCenter(eingabeContent);
             //stage.setTitle("noScrabble");
             //stage.setScene(new Scene(root, 1024, 1024));
             //stage.show();
