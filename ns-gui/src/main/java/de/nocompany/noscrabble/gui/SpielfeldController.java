8fad1f3fb89e1ad8423c41460dbcaae5604294df
Dietrich <waldschmidt92@googlemail.com>
2024-02-15 14:20:10 +0100

    TODOS eingefügt

--- ns-gui/src/main/java/de/nocompany/noscrabble/gui/SpielfeldController.java
+++ ns-gui/src/main/java/de/nocompany/noscrabble/gui/SpielfeldController.java
@@ -1,6 +1,7 @@
 package de.nocompany.noscrabble.gui;
 
 import de.nocompany.noscrabble.service.serviceSpiel.ServiceSpielImpl;
+import de.nocompany.noscrabble.service.serviceSpiel.ServiceSpielInterface;
 import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -14,6 +15,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SpielfeldController {
+
+    private final ServiceSpielInterface spielService = new ServiceSpielImpl();
+
 
     private List<Pane> draggableObjects = new ArrayList<>(); // Liste der draggable Objekte
 
@@ -45,10 +49,11 @@ public class SpielfeldController {
     private void initialize() {
         setupDraggableObjects();
     }
-    //final SpielService = new ServiceSpielImpl();
+
 
     public void erzeugeNeuesSpiel(List<String> spielerListe) {
         System.out.println("Empfange Spieler: "+spielerListe);
+        spielService.neuesSpiel(spielerListe);
 
     }
     private int konvertiereZuSpielKoordinate(double pixelKoordinate) {
@@ -175,6 +180,7 @@ public class SpielfeldController {
             auswertenButton.setStyle("-fx-text-fill: -fx-text-base-color; -fx-background-color: transparent;");
         });
         pause.play();
+        //TODO spielService.pruefeWoerter()
     }
 
     public void neuButton() {
@@ -184,8 +190,7 @@ public class SpielfeldController {
             neuButton.setStyle("-fx-text-fill: -fx-text-base-color; -fx-background-color: transparent;");
         });
         pause.play();
-    }
-
-    public void setListe(List<String> spielerListe) {
+        //TODO spielService.ersetzeBuchstaben();
+        spielService.passeRunde();
     }
 }
