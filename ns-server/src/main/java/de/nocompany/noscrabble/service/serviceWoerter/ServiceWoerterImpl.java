52d72abaaa7f3a7f468ed0faab4e8ae77df11a5d
AlexPC <mytrashmail0100@gmail.com>
2024-02-15 09:53:03 +0100

    Impl Tests funktionieren

--- ns-server/src/main/java/de/nocompany/noscrabble/service/serviceWoerter/ServiceWoerterImpl.java
+++ ns-server/src/main/java/de/nocompany/noscrabble/service/serviceWoerter/ServiceWoerterImpl.java
@@ -1,102 +1,90 @@
 package de.nocompany.noscrabble.service.serviceWoerter;
 
 
-//import de.nocompany.noscrabble.model.punktemultiplikator.Punktemultiplikator;
+import de.nocompany.noscrabble.dao.daoWoerter.WoerterDaoImpl;
+import de.nocompany.noscrabble.model.punktemultiplikator.Punktemultiplikator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServiceWoerterImpl implements ServiceWoerterInterface {
+    public int punkte;
+    private WoerterDaoImpl dao = new WoerterDaoImpl();
+    private Punktemultiplikator punktemultiplikator = new Punktemultiplikator();
 
-    //  private Punktemultiplikator punktemultiplikator = new Punktemultiplikator();
-
-    public static void main(String[] args) {
-        ServiceWoerterInterface test = new ServiceWoerterImpl();
-        Character[][] altBrett = {
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', 'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', 'W', 'E', 'B', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', 'O', ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', 'J', 'A', 'V', 'A', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', 'D', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', 'E', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'G', 'A', 'M', 'E', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
-        };
-
-        Character[][] neuBrett = {
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', 'H', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', 'W', 'E', 'B', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', 'O', ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', 'J', 'A', 'V', 'A', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', 'D', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', 'E', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', 'a', ' ', ' ', ' ', ' ', ' ', 'G', 'A', 'M', 'E', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
-        };
-
-        System.out.println(test.calcNewWordPoints(altBrett, neuBrett));
-
+    public int getPunkte() {
+        return punkte;
     }
 
     @Override
-    public String calcNewWordPoints(Character[][] altBrett, Character[][] neuBrett) {
-        String neuesWort = "";
-        int startX = -1;
-        int startY = -1;
-        boolean horizontal = false;
+    public boolean calcNewWordPoints(Character[][] altBrett, Character[][] neuBrett) {
+        int gesamtPunkte = 0;
+        List<String> foundWords = new ArrayList<>();
 
-        outerLoop:
         for (int y = 0; y < neuBrett.length; y++) {
             for (int x = 0; x < neuBrett[y].length; x++) {
-                if (!neuBrett[y][x].equals(altBrett[y][x])) {
-                    startX = x;
-                    startY = y;
+                if (!neuBrett[y][x].equals(altBrett[y][x]) && neuBrett[y][x] != ' ') {
+                    String[] potenzielleNeueWorte = {
+                            extrahiereWort(neuBrett, x, y, true),  // Horizontal
+                            extrahiereWort(neuBrett, x, y, false)  // Vertikal
+                    };
 
-                    if (x + 1 < neuBrett[y].length && !neuBrett[y][x + 1].equals(altBrett[y][x + 1])) {
-                        horizontal = true;
-                    } else if (y + 1 < neuBrett.length && !neuBrett[y + 1][x].equals(altBrett[y + 1][x])) {
-                        horizontal = false;
-                    }
-
-                    if (horizontal) {
-                        if (startX > 0 && neuBrett[y][startX - 1] != ' ') {
-                            for (int i = startX - 1; i >= 0 && neuBrett[y][i] != ' '; i--) {
-                                startX = i;
-                            }
-                        }
-                        for (int i = startX; i < neuBrett[y].length && neuBrett[y][i] != ' '; i++) {
-                            neuesWort += neuBrett[y][i];
-                        }
-                    } else {
-                        if (startY > 0 && neuBrett[startY - 1][x] != ' ') {
-                            for (int i = startY - 1; i >= 0 && neuBrett[i][x] != ' '; i--) {
-                                startY = i;
-                            }
-                        }
-                        for (int i = startY; i < neuBrett.length && neuBrett[i][x] != ' '; i++) {
-                            neuesWort += neuBrett[i][x];
+                    for (String wort : potenzielleNeueWorte) {
+                        if (isValidNewWord(wort, altBrett, x, y, foundWords.contains(wort))) {
+                            foundWords.add(wort);
+                            gesamtPunkte += punktemultiplikator.calcWortPunkte(wort, x, y, wort.equals(potenzielleNeueWorte[0]));
                         }
                     }
-                    break outerLoop;
                 }
             }
         }
-//        if (!neuesWort.isEmpty()) {
-//            return punktemultiplikator.calcWortPunkte(neuesWort, startX, startY, horizontal);
-//        } else {
-//            return 0;
-//        }
-        return neuesWort;
+        System.out.println(foundWords);
+        punkte = gesamtPunkte;
+        for (String elem : foundWords) {
+            if (!dao.checkWord(elem)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String extrahiereWort(Character[][] brett, int startX, int startY, boolean horizontal) {
+        String wort = "";
+        int x = startX;
+        int y = startY;
+
+        if (horizontal) {
+            while (x > 0 && brett[y][x - 1] != ' ') {
+                x--;
+            }
+            while (x < brett[y].length && brett[y][x] != ' ') {
+                wort += brett[y][x];
+                x++;
+            }
+        } else {
+            while (y > 0 && brett[y - 1][x] != ' ') {
+                y--;
+            }
+            while (y < brett.length && brett[y][x] != ' ') {
+                wort += brett[y][x];
+                y++;
+            }
+        }
+        return wort;
+    }
+
+    public boolean isValidNewWord(String wort, Character[][] altBrett, int x, int y, boolean alreadyFound) {
+        if (wort.isEmpty() || alreadyFound || wort.length() == 1) return false;
+
+        for (int i = 0; i < wort.length(); i++) {
+            int prufX = x + i;
+            int prufY = y;
+            if (prufX >= 0 && prufX < altBrett[0].length && prufY >= 0 && prufY < altBrett.length) {
+                if (!altBrett[prufY][prufX].equals(wort.charAt(i))) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
